cf60f50ea196a58429baf9732834949922a7ed96
denzven <denzvenvadakkan@gmail.com>
2021-12-03 21:03:47 +0530

    Update Day3part2.py

--- day3/Day3part2.py
+++ day3/Day3part2.py
@@ -1,28 +1,58 @@
+from time import sleep
+
 InputFile = open('Day3Input.txt')
 InputFileLines = InputFile.readlines()
-InputArray = [str(line) for line in InputFileLines]
+InputArray = [str(int(line)) for line in InputFileLines]
+InputArray.sort(reverse=True) 
+#print(InputArray)
+#sleep(5)
 IniArray = [1,2,3,4,5,6,7,8,9,10,11,12]
 ones = 0
 zeros = 0
 i = 0
-Array = []
-Array2 = []
+ArrayOnes = []
+ArrayZeros = []
 
-for i in IniArray:
-	for number in InputArray:
-		print(int(number[i:i+1]))
-		if int(number[i:i+1]) == 1:
-			print(int(number[i:i+1]))
-			ones += 1
-			i += 1
-			Array2 = Array2.append(InputArray[i])
-			print(Array2)
+InputArray = ['000110000001','011011001101','001101100111','111101010010']
+def looper():
+    global InputArray
+    global IniArray
+    global i
 
-		if int(number[i:i+1]) == 0:
-			zeros += 1
-			i += 1
-			Array2 = Array2.append(InputArray[i])
-			print(Array2)
+    ones = 0
+    zeros = 0
+    ArrayOnes = []
+    ArrayZeros = []
 
-if ones > zeros:
-	Array2.append()
+    for q in IniArray:
+        if i > 11:
+            break
+        for number in InputArray:
+            print(number)
+            print(i)
+            print(int(number[i:i+1]))
+            if number[i:i+1] == '':
+                break
+            if int(number[i:i+1]) == 1:
+                print(int(number[i:i+1]))
+                ones += 1
+                ArrayOnes.append(number)
+                pass    
+        
+            elif int(number[i:i+1]) == 0:
+                print(int(number[i:i+1]))
+                zeros += 1
+                ArrayZeros.append(number)
+                pass
+        
+            print(f"ArrayOnes: {ArrayOnes}\n\n\n")
+            #sleep(1)
+            print(f"ArrayZeros: {ArrayZeros}\n\n\n")
+    i+=1
+    if ones < zeros:
+        InputArray = ArrayOnes
+    if ones > zeros:
+        InputArray = ArrayZeros
+    sleep(5)
+    looper()
+looper()
