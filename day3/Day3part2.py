dca76f15e66a99baf68798ab6085acea54d20644
denzven <denzvenvadakkan@gmail.com>
2021-12-03 13:33:43 +0530

    Update Day3part2.py

--- day3/Day3part2.py
+++ day3/Day3part2.py
@@ -1,1 +1,28 @@
-# WIP
+InputFile = open('Day3Input.txt')
+InputFileLines = InputFile.readlines()
+InputArray = [str(line) for line in InputFileLines]
+IniArray = [1,2,3,4,5,6,7,8,9,10,11,12]
+ones = 0
+zeros = 0
+i = 0
+Array = []
+Array2 = []
+
+for i in IniArray:
+	for number in InputArray:
+		print(int(number[i:i+1]))
+		if int(number[i:i+1]) == 1:
+			print(int(number[i:i+1]))
+			ones += 1
+			i += 1
+			Array2 = Array2.append(InputArray[i])
+			print(Array2)
+
+		if int(number[i:i+1]) == 0:
+			zeros += 1
+			i += 1
+			Array2 = Array2.append(InputArray[i])
+			print(Array2)
+
+if ones > zeros:
+	Array2.append()
